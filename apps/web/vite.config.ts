55c68e40188349876f79ae77fc0f9c5b23d7d78a
杨福绅 <fushenyang@gmail.com>
2026-03-06 21:06:57 +0800

    提交

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,6 +1,15 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
 
 export default defineConfig({
   plugins: [solid()],
-})
+  server: {
+    proxy: {
+      // 告诉 Vite：只要是发往 /posts 的请求，全部转发给 3000 端口
+      "/posts": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
+  },
+});
